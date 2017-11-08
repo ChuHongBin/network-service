ac37559f21bc36f7c2ebcad7bc05c14d6fd6d495
779504683@qq.com <123>
2017-11-08 18:16:13 +0800

    图片传输成功

--- app/src/main/java/com/example/network_service/MainActivity.java
+++ app/src/main/java/com/example/network_service/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.network_service;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -18,6 +24,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,35 +34,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private TextView text;
 
-    private static final String HOST = "192.168.103.2";
+    private static final String HOST ="192.168.103.2"; //"113.17.169.50";
 
-    private static final int POST = 881;
+    private static final int POST = 1234;//30792;
 
     private Socket socket;
 
-    private Boolean connectSign=true;
+    private BufferedInputStream reader;
 
-    private BufferedReader reader;
-
-    private BufferedWriter writer;
+    private BufferedOutputStream writer;
 
     private static final int UPDATE_TEXT = 1;
 
     private String response = null;
 
-    private static final int CONNECTSIGN = 2;
+    private Bitmap bitmap;
+
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         connect = (Button) findViewById(R.id.connect);
         text = (TextView) findViewById(R.id.initText);
+        imageView = (ImageView) findViewById(R.id.image_view);
         connect.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
         //连接
-        if(v.getId()==R.id.connect && true == connectSign){ //
+        if(v.getId()==R.id.connect){ //
              connectRequest();
         }
     }
@@ -66,10 +74,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void handleMessage(Message msg){
             switch (msg.what){
                 case UPDATE_TEXT:
-                    text.setText(response);
+                    text.setText("成功");
+                    imageView.setImageBitmap(bitmap);
                     break;
-                case CONNECTSIGN:
-                    Toast.makeText(MainActivity.this, "连接超时", Toast.LENGTH_SHORT).show();
+                case 2 :
+                    Toast.makeText(MainActivity.this, "等待接收信息..", Toast.LENGTH_SHORT).show();
+                case 3 :
+                    Toast.makeText(MainActivity.this, "连接超时..", Toast.LENGTH_SHORT).show();
             }
         }
     };
@@ -87,55 +98,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void run() {
                 try {
-                    connectSign =false;
+
                     socket = new Socket();
                     SocketAddress socketAddress = new InetSocketAddress(HOST, POST);
                     //3秒超时
                     socket.connect(socketAddress,3000);
                     //获取数据流
-                    reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    startServerReplyListener(reader);
-                    writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                    writer.write("admin123\n");
+                    reader = new BufferedInputStream(socket.getInputStream());
+                    writer = new BufferedOutputStream(socket.getOutputStream());
+                    writer.write("android".getBytes("UTF-8"));
                     writer.flush();
-                    timeSend(writer);
-                } catch (Exception e) {
-                    connectSign = true;
-                    setMessage(CONNECTSIGN);
+                    startServerReplyListener(reader);
+                }catch (SocketTimeoutException e){
+                    setMessage(3);
+                } catch (IOException ex) {
+
                 }
             }
         }).start();
     }
-    //定时发送
-    public void timeSend(final BufferedWriter writer){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                    new Timer().schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            try {
-                                writer.write("hello world .. \n");
-                                writer.flush();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    },3000,3000);
-                }
-        }).start();
-    }
 
     //接收数据
-    public void startServerReplyListener(final BufferedReader reader){
+    public void startServerReplyListener(final BufferedInputStream reader){
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    while((response = reader.readLine()) != null){
-                        setMessage(UPDATE_TEXT);
+                    byte[] img = new byte[1024 * 11];
+                    byte[] buff = new byte[1024 * 2];
+                    byte[] begin = new byte[3];
+                    byte[] end = new byte[3];
+                    int num = 0;
+                    int i = 0;
+                    while(true){
+                        boolean istrue = false;
+                        while((num = reader.read(buff)) != -1){
+                            //判断尾部
+                            if(num < 1024*2){
+                                System.arraycopy(buff, num-3, end, 0, 3);
+                                String endString = new String(end);
+                                if(endString.equals("end")){
+                                    num = num - 3;
+                                    istrue = true;
+                                    setMessage(2);
+                                }else {
+                                    i = 0;
+                                }
+                            }
+                            System.arraycopy(buff,0, img, i*1024*2, num);
+                            if(istrue == true){
+                                bitmap = BitmapFactory.decodeByteArray(img, 0, i*1024*2+num);
+                                i=0;
+                                setMessage(UPDATE_TEXT);
+                                istrue = false;
+                            }
+                            i++;
+                        }
                     }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -147,7 +167,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onDestroy(){
         super.onDestroy();
         try {
-            writer.write("bye");
             writer.flush();
             socket.close();
             reader.close();
